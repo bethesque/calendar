a6c5eb6641c3470cf1d0b5bf953f6d7aa8b5cf92
Beth Skurrie <beth@bethesque.com>
2026-03-19 09:33:28 +1100

    fix: add missing force argument

--- index.py
+++ index.py
@@ -41,7 +41,7 @@ class CalendarWebServer(object):
         with open("token.json", "w") as text_file:
             print(flow.credentials.to_json(), file=text_file)
 
-        run(hardware_render, CalendarSource(stubbed=True), 1304, 984)
+        run(hardware_render, CalendarSource(stubbed=True), 1304, 984, true)
 
         return "Welcome back"
 
