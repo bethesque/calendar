51d170ce71923e062c37d896261f488e96e870a2
Beth Skurrie <beth@bethesque.com>
2026-03-19 10:12:23 +1100

    chore: add -e flag to scripts

--- main-force.sh
+++ main-force.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # Runs once every few minutes, forcing update if the force.txt file has been created by a restart or a credential update
-
-# Make this work on mac and on the raspberry pi. Not sure why/if it needs the absolute path, but don't want to mess with it.
-if [ -x "/usr/bin/python" ]; then
-  PYTH="/usr/bin/python"
-else
-  PYTH="python"
-fi
-
 if [ -f /tmp/force.txt ]; then
   # Force re-render of the image
   rm -rf /tmp/force.txt
+
+  # Make this work on mac and on the raspberry pi.
+  if [ -x "/usr/bin/python" ]; then
+    PYTH="/usr/bin/python"
+  else
+    PYTH="python"
+  fi
+
   echo "Executing main with --force"
   "$PYTH" main.py --force >> log.txt
 fi
--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # Runs once an hour, updating the calendar if any events have changed
 
-# Make this work on mac and on the raspberry pi. Not sure why/if it needs the absolute path, but don't want to mess with it.
+# Make this work on mac and on the raspberry pi.
 if [ -x "/usr/bin/python" ]; then
   echo "/usr/bin/python exists and is executable."
   PYTH="/usr/bin/python"
