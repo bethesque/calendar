a6e3064f0fdcd0bedd9dca31db536f95fec56205
Beth Skurrie <beth@bethesque.com>
2026-03-19 11:10:11 +1100

    chore: adding log statements

--- main.py
+++ main.py
@@ -55,13 +55,16 @@ def load_image(calendar_source, surface, force):
         with open(DATA_FILE, "w") as f:
             f.write('["credentials"]')
         if last_render == '["credentials"]':
+            print("last_render == '["credentials"]' whatever that means")
             return
         print("unable to load creds, rendering qr code for re-auth")
         return make_qr_code(SERVER_ADDRESS, surface)
     else:
+        print("loading calendars")
         calendars = calendar_source.load_data(creds, filter)
         data_json = json.dumps(calendars, sort_keys=True, default=json_default_encoder)
         if last_render == data_json and not force:
+            print("Calendar data has not changed, and force is not true, so returning None")
             return
         with open(DATA_FILE, "w") as f:
             f.write(data_json)
@@ -82,7 +85,10 @@ if __name__ == "__main__":
     force = (len(sys.argv) > 1 and sys.argv[1] == "--force")
     print("running: " + datetime.now().isoformat() + " force=" + str(force), flush=True)
     if IS_LOCAL:
+        print("Rendering locally")
         run(local_render, CalendarSource(stubbed=True), 1304, 984, force)
     else:
+        print("Refreshing hardware screen")
         run(hardware_render, CalendarSource(stubbed=False), 1304, 984, force)
+    print("finished")
 
