601ae3451223689cbca22c85cbab3a889a42d7f1
Beth Skurrie <beth@bethesque.com>
2026-03-19 12:17:05 +1100

    feat: add script to force refresh when tokens updated

---
+++ main-update-screen-if-token-updated.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+# Force sceen update if the token file has been updated more recently than the calendar was last rendered.
+if [ -f token.json ] && { [ ! -f ecalendar-last-render.json ] || [ token.json -nt ecalendar-last-render.json ]; }; then
+  touch ecalendar-last-render.json
+
+  # Make this work on mac and on the raspberry pi.
+  if [ -x "/usr/bin/python" ]; then
+    PYTH="/usr/bin/python"
+  else
+    PYTH="python"
+  fi
+
+  echo "Executing main with --force"
+  "$PYTH" main.py --force >> log.txt
+else
+  echo "calendar rendered more recently than tokens fetched"
+fi
