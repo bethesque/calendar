6c6c9b72d90d10367cb939fed3a0dcf1cf6de858
Beth Skurrie <beth@bethesque.com>
2026-03-20 11:21:41 +1100

    chore: set back to black

--- layout.py
+++ layout.py
@@ -15,7 +15,7 @@ LEFT_WIDTH = 70
 
 def layout_calendars(calendars: list[CalendarDay], surface):
     image = Image.new(
-        "RGB", (surface.right, surface.bottom), surface.RED
+        "RGB", (surface.right, surface.bottom), surface.BLACK
     )  # 255: clear the frame
     draw = ImageDraw.Draw(image)
     box = EqualChildrenBox(padding=5, stroke=0)
@@ -40,7 +40,7 @@ def layout_calendars(calendars: list[CalendarDay], surface):
                     margin=2,
                     left_width=LEFT_WIDTH,
                     left=Text(event.owner),
-                    right=Text(event.summary, font=summary_font, color=RED),
+                    right=Text(event.summary, font=summary_font, color=BLACK),
                 )
             )
         for event in day.timed_events:
@@ -54,7 +54,7 @@ def layout_calendars(calendars: list[CalendarDay], surface):
                     left=Text(
                         f"{event.owner}\n{event.start_time.strftime('%I:%M %p')}"
                     ),
-                    right=Text(event.summary, font=summary_font, padding_top=4, color=RED),
+                    right=Text(event.summary, font=summary_font, padding_top=4, color=BLACK),
                 )
             )
 
