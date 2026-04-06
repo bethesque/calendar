d1666fc864f46c31796480d9b328e7e750b6337e
Beth Skurrie <beth@bethesque.com>
2026-04-06 10:52:23 +1000

    chore: start fading from current vol, not 100

--- mpv_stop_alarm.py
+++ mpv_stop_alarm.py
@@ -16,6 +16,32 @@ def send_command(ipc_socket, cmd, args=None):
     except (ConnectionRefusedError, FileNotFoundError):
         print("mpv is not running or IPC socket missing")
 
+def get_property(ipc_socket, property_name):
+    """Get a property value from mpv."""
+    message = json.dumps({"command": ["get_property", property_name]}) + "\n"
+    try:
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.connect(ipc_socket)
+            s.sendall(message.encode("utf-8"))
+            # Read response
+            response = b""
+            while True:
+                chunk = s.recv(1024)
+                if not chunk:
+                    break
+                response += chunk
+                if b"\n" in response:
+                    break
+            try:
+                data = json.loads(response.decode("utf-8").strip())
+                if "data" in data:
+                    return data["data"]
+            except json.JSONDecodeError:
+                pass
+    except (ConnectionRefusedError, FileNotFoundError):
+        print("mpv is not running or IPC socket missing")
+    return None
+
 def stop_alarm(ipc_socket):
     send_command(ipc_socket, "stop")
 
@@ -23,8 +49,13 @@ def set_volume(ipc_socket, vol):
     send_command(ipc_socket, "set_property", ["volume", vol])
 
 def fade_out(duration=2.0, steps=10):
+    # Get current volume from alarm socket
+    current_volume = get_property(ALARM_SOCKET, "volume")
+    if current_volume is None:
+        current_volume = 50  # fallback if we can't get current volume
+    
     step_time = duration / steps
-    for vol in reversed(range(0, 101, 100 // steps)):
+    for vol in reversed(range(0, int(current_volume) + 1, int(current_volume) // steps)):
         set_volume(ALARM_SOCKET, vol)
         set_volume(ANNOUNCEMENT_SOCKET, vol)
         time.sleep(step_time)
