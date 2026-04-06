4d9c7e63de074ba180168bca61d5049c3dea18bc
Beth Skurrie <beth@bethesque.com>
2026-04-06 14:02:01 +1000

    chore: refactor

--- mpv/mpv.py
+++ mpv/mpv.py
@@ -4,6 +4,7 @@ import json
 import socket
 import os
 import logging
+import time
 
 """
 Manages the mpv process for playing alarm and announcement sounds. Uses mpv's IPC interface to control playback and volume.
@@ -129,3 +130,24 @@ class MpvProcess:
 
     def stop(self):
         self.send_command("stop")    
+
+def fade_out(mvp_processes, duration=2.0, steps=10):
+    """
+    Gradually fade out the volume of the given mpv processes over the specified duration and steps, then stop them.
+    """
+    
+    processes_to_fade = []
+    for player in mvp_processes:
+        volume = int(volume) if (volume := player.get_property("volume")) is not None else None
+        if volume is not None:
+            processes_to_fade.append((player, volume))
+
+    step_time = duration / steps
+
+    for percent_vol in reversed(range(0, 100, 100 // steps)):
+        for player, volume in processes_to_fade:
+            player.set_volume(volume * percent_vol // 100)
+        time.sleep(step_time)
+    
+    for player in mvp_processes:
+        player.stop()
--- mpv/mpv_stop_alarm.py
+++ mpv/mpv_stop_alarm.py
@@ -1,6 +1,7 @@
 import time
 import logging
-from mpv import MpvProcess
+from mpv import MpvProcess, fade_out
+
 
 logging.basicConfig(
         level=logging.DEBUG,
@@ -13,27 +14,6 @@ logging.basicConfig(
 
 ALARM_SOCKET = "/tmp/mpv_alarm.sock"
 ANNOUNCEMENT_SOCKET = "/tmp/mpv_announcement.sock"
-
-def fade_out(mvp_processes, duration=2.0, steps=10):
-    """
-    Gradually fade out the volume of the given mpv processes over the specified duration and steps, then stop them.
-    """
-    
-    processes_to_fade = []
-    for player in mvp_processes:
-        volume = int(volume) if (volume := player.get_property("volume")) is not None else None
-        if volume is not None:
-            processes_to_fade.append((player, volume))
-
-    step_time = duration / steps
-
-    for percent_vol in reversed(range(0, 100, 100 // steps)):
-        for player, volume in processes_to_fade:
-            player.set_volume(volume * percent_vol // 100)
-        time.sleep(step_time)
-    
-    for player in mvp_processes:
-        player.stop()
 
 if __name__ == "__main__":
     alarm_player = MpvProcess(ALARM_SOCKET)
