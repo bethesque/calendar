57e43c32b61cd94448418a7657fad1f98940885a
Beth Skurrie <beth@bethesque.com>
2026-04-09 07:54:56 +1000

    fix: fix qr code bug

--- qr.py
+++ qr.py
@@ -11,7 +11,7 @@ def make_qr_code(url, surface):
     qr.add_data(url)
     qr.make(fit=True)
 
-    qr_image = qr.make_image(fill_color=surface.BLACK, back_color=surface.WHITE)
+    qr_image = qr.make_image(fill_color=surface.BLACK, back_color=surface.WHITE).convert("1")
     qr_w, qr_h = qr_image.size
     s_w = surface.right - surface.left
     s_h = surface.bottom - surface.top
@@ -20,6 +20,7 @@ def make_qr_code(url, surface):
     if px < 0 or py < 0:
         print(f"Oh oh, image is {qr_w}x{qr_h} and doesn't fit surface {surface}!")
         return qr_image
+    print(type(qr_image))
     result = Image.new(qr_image.mode, (s_w, s_h), surface.WHITE)
-    result.paste(qr_image, (px, py))
+    result.paste(qr_image, (px, py, px + qr_w, py + qr_h))
     return result
--- tests/manual/show_qr_code_test.py
+++ tests/manual/show_qr_code_test.py
@@ -5,10 +5,14 @@ sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..", "..")))
 from qr import make_qr_code
 from model import Surface
 from screen import hardware_render
+from env import IS_LOCAL
 
 
 if __name__ == "__main__":
     url = "https://www.google.com/calendar"
     surface = Surface(0, 0, 1304, 984)
     image = make_qr_code(url, surface)
-    hardware_render(image)
+    if IS_LOCAL:
+        image.show("test")
+    else:
+        hardware_render(image)
