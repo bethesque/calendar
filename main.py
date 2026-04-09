943828277bb208d352dc6174507c5256880e44ce
Beth Skurrie <beth@bethesque.com>
2026-04-09 16:34:36 +1000

    chore: move layout.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ecal.google_calendar import CalendarSource
-from layout import layout_calendars
+from ecal.screen.layout import layout_calendars
 from model import Surface
 from ecal.screen.hardware_screen import hardware_render
 from ecal.screen.qr import make_qr_code
